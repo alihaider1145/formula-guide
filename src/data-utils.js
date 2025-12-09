248621e2c34302e1aadda038bec26f45784ba2c8
alihaider1145 <alihaider15112006@gmail.com>
2025-12-09 21:33:04 +0500

    Fetch JSON files from raw github instead of local files

--- src/data-utils.js
+++ src/data-utils.js
@@ -1,13 +1,13 @@
 const getFetchURL = async (subject, grade, topic, chapter) => {
-    // const owner = "alihaider1145";
-    // const repo = "formula-guide";
-    // const directoryPath = `dist/assets/${subject}/${topic}/${grade}/${chapter}.json`;
+    const owner = "alihaider1145";
+    const repo = "formula-guide";
+    const directoryPath = `/dist/assets/${subject}/${grade}/${topic}/${chapter}.json`;
 
-    // const apiUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${directoryPath}`;
+    const apiUrl = `https://raw.githubusercontent.com/${owner}/${repo}/main/${directoryPath}`;
 
-    // return  apiUrl;
-    const fetchURL = `/assets/${subject}/${grade}/${topic}/${chapter}.json`;
-    return fetchURL;
+    return  apiUrl;
+    // const fetchURL = `/assets/${subject}/${grade}/${topic}/${chapter}.json`;
+    // return fetchURL;
 }
 
 const  fetchData = async (jsonFilePath) => {
